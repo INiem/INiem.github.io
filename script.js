11434e95a4586db063274eaff448f35f3d30dc36
INiem <60138558+INiem@users.noreply.github.com>
2020-02-24 13:32:20 +0200

    muutos

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
     $(".Work").hide(); //hide your div initially
     var topOfOthDiv = $("#tyot").offset().top;
     $(window).scroll(function() {
-        if($(window).scrollTop() > topOfOthDiv - 200) { //scrolled past the other div?
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
             $(".Work").show(); //reached the desired point -- show div
         }else{
             $(".Work").hide();
